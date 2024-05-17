287b64f05778d4c6a11f80fa7cfa28852b253aa3
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-17 23:25:08 +0300

    feat: add view transition on appearance switch

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,9 +4,11 @@ import "virtual:uno.css";
 import "./style.css";
 import type { EnhanceAppContext } from "vitepress";
 import Theme from "vitepress/theme";
+import Layout from "./Layout.vue";
 
 export default {
 	extends: Theme,
+	Layout,
 	enhanceApp({ app }: EnhanceAppContext) {
 		app.use(TwoslashFloatingVue);
 	},
